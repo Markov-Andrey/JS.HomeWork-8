edc2ac2e4ba92f029d93d0ba7ab18487a437492c
Markov-Andrey <andremarkov@icloud.com>
2022-10-29 19:19:49 +0300

    total refactoring
    code structure changed, functions are arranged in blocks

--- script/main.js
+++ script/main.js
@@ -1,39 +1,36 @@
-/*
-КЛАСС: КОНФИГУРАТОР ЧАЙНИКА
-//Фичи для реализации:
+//БЛОК ЦВЕТ ЧАЙНИКА
 
-СВОЙСТВА:
-ГОТОВО! Цвет чайника из списка (ЛЕГКОМАСШТАБИРУЕМО!)
-Марка чайника (Модель чайника) (ЛЕГКОМАСШТАБИРУЕМО!)
-Объем чайника
-Мощность чайника
-
-ОПЦИОНАЛЬНО:
-Термометр в чайнике
-Защита от "пустого" чайника
-Индикатор работы чайника
-
-ЗАДАЧА:
-Создать структуру объекта через логику ООП. 
-Постоянное масштабирование объекта без переписывания кода.
-Стуктура подобного объекта?
-*/
-
-//БЛОК ЦВЕТ
-//Функция смены цвета чайника
-let colorKettel = color => {
-	kettel = document.querySelector('#electric-kettle');
-	kettel.style.fill = color;
-};
-//База цветов чайника
+//Список переменных
 let colorCode = []; //код цвета
 let colorName = []; //наименование цвета
-//Функция заполнения массива значениями - код цвета, наменование цвета
+let selectColor = []; //добавление значений в список цветов
+let i; //индекс для расчетов
+const selectIdColor = document.querySelector('#selectColor');//поиск блока по id в HTML
+const selectIdBrand = document.querySelector('#celectBrand');//поиск блока по id в HTML
+
+//Функция смены цвета чайника
+let colorKettel = color => {
+	let kettel = document.querySelector('#electric-kettle');
+	kettel.style.fill = color;
+};
+
+//Заполнение массива значениями (код, наменование цвета) и пополнение списка
 let addColor = (code,name) => {
 	colorCode.push(code)
 	colorName.push(name)
-}
-//Заполнение массива кодом и наименованием цвета
+	i = colorCode.length - 1; //поиск индекса последней записи
+	selectColor[i] = document.createElement('option');
+	selectColor[i].innerHTML = colorName[i];
+	selectIdColor.appendChild(selectColor[i]);
+};
+
+//Подбор значения цвета чайника из списка
+selectIdColor.addEventListener('change', () => {
+	i = selectIdColor.selectedIndex; //поиск индекса выбранной записи
+	colorKettel(colorCode[i]); //вызов покраски
+});
+
+//Заполнение массива (код, наименование цвета)
 addColor ('Black','Ночной чёрный');
 addColor ('Crimson','Насыщенный красный');
 addColor ('Tomato','Оранжевый томатный');
@@ -43,26 +40,15 @@ addColor ('DeepSkyBlue','Небесный голубой');
 addColor ('RoyalBlue','Королевский синий');
 addColor ('Indigo','Индиго');
 addColor ('Pink','Нежно-розовый');
-//Автозаполнение списка с цветами чайника
-let selectColor = [];
-for (let i = 0; i < colorName.length;i++){
-	selectColor[i] = document.createElement('option');
-	selectColor[i].innerHTML = colorName[i];
-	document.querySelector('#selectColor').appendChild(selectColor[i])
-}
-//Подбор значений цвета чайника из списка <option>
-let selectElem = document.querySelector('#selectColor');
-selectElem.addEventListener('change', () => {
-	let index = selectElem.selectedIndex;
-	colorKettel(colorCode[index]);
-});
 
 //БЛОК БРЕНД
-// SVG-логотипы подхватываются из др.JS-файла
+
+//Список переменных
+//SVG-логотипы чайников (из стороннего JS-файла)
 let logoSmeg = logo_Smeg();
 let logoBraun = logo_Braun();
 let logoXiaomi = logo_Xiaomi();
-//Модели чайников
+//SVG-Модели чайников (из стороннего JS-файла)
 let model1 = model_1();
 let model2 = model_2();
 let model3 = model_3();
@@ -70,37 +56,35 @@ let model3 = model_3();
 let brandCode = []; //наименование цвета
 let brandName = []; //наименование бренда
 let brandModel = []; //наименование модели
-//Функция заполнения массива значениями - наменование бренда
+let selectBrand = []; //добавление значений в список брендов
+//поиск класса
+let logoClass;
+let kettelClass;
+
+//Заполнение массива значениями (логотип, название бренда, изображение модели) и пополнение списка
 let addBrand = (code,brand,model) => {
 	brandCode.push(code)
 	brandName.push(brand)
 	brandModel.push(model)
-};
-//заполенение массива - логотип, название бренда, изображение модели
-addBrand(logoSmeg,'Smeg',model1);
-addBrand(logoBraun,'Braun',model2);
-addBrand(logoXiaomi,'Xiaomi',model3);
-//Автозаполнение списка с брендами чайника
-let selectBrand = [];
-for (let i = 0; i < brandName.length;i++){
+	i = brandName.length - 1; //поиск индекса последней записи
 	selectBrand[i] = document.createElement('option');
 	selectBrand[i].innerHTML = brandName[i];
-	document.querySelector('#celectBrand').appendChild(selectBrand[i])
+	selectIdBrand.appendChild(selectBrand[i])
 };
+
 //Подбор значений марки чайника из списка <option>
-let selectElem2 = document.querySelector('#celectBrand')
-selectElem2.addEventListener('change', () => {
-	let index2 = selectElem2.selectedIndex;
-	logoKettel(brandName[index2],brandCode[index2],brandModel[index2]);
+selectIdBrand.addEventListener('change', () => {
+	i = selectIdBrand.selectedIndex;
+	logoKettel(brandName[i],brandCode[i],brandModel[i]);
 });
 //функция выбора названия бренда и модели
 let logoKettel = (brandName,brandCode,brandModel) => {
 	//прочистка документа от логотипа
-	let logoClass = document.querySelector('.logo');
-	logoClass ? logoClass.remove() : false;
+	logoClass = document.querySelector('.logo');
+	if (logoClass) logoClass.remove();
 	//прочистка от изображения чайника
-	let kettelClass = document.querySelector('.center');
-	kettelClass ? kettelClass.remove() : false;
+	kettelClass = document.querySelector('.center');
+	if (kettelClass) kettelClass.remove();
 	//создание изображения чайника
 	kettelImage = document.createElement('p');
 	kettelImage.classList.add ('center');
@@ -108,18 +92,16 @@ let logoKettel = (brandName,brandCode,brandModel) => {
 	document.querySelector('#div1').prepend(kettelImage);
 	//создание логотипа
 	logo = document.createElement('p');
-	logo.classList.add (`logo${brandName}`);
-	logo.classList.add ('logo');
+	logo.classList.add (`logo${brandName}`);//добавление специального класса CSS
+	logo.classList.add ('logo');//класс-пустышка для поиска
 	logo.innerHTML = brandCode;
 	document.querySelector('#div1').prepend(logo);
 	//выбор цвета при выборе нового бренда
-	index = selectElem.selectedIndex;
-	colorKettel(colorCode[index]);
+	i = selectIdColor.selectedIndex;
+	colorKettel(colorCode[i]);
 };
 
-//Конфигуратор чайника
-//class Kettel {
-//	constructor(){
-//		
-//	};
-//};
+//Заполнение массива (логотип, название бренда, изображение модели)
+addBrand(logoSmeg, 'Smeg', model1);
+addBrand(logoBraun, 'Braun', model2);
+addBrand(logoXiaomi, 'Xiaomi', model3);
